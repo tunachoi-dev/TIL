eb86c8f4305d80f212d8a07996762a8fe964695b
tunachoi-dev <227953280+tunachoi-dev@users.noreply.github.com>
2025-09-18 23:33:54 +0900

    [Java]Variable

---
+++ Java/Variable/example/VariableEx1.java
@@ -0,0 +1,9 @@
+public class VariableEx1 {
+    public static void main(String[] args) {
+        String name = "tunachoi-dev";
+        int year = 2025;
+
+        System.out.println("My name is " + name);
+        System.out.println("The year is " + year);
+    }
+}
